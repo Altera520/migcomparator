9a0b2de011f07785dc08085142c17c4331b22353
altera520 <sencom1028@gmail.com>
2021-12-31 17:15:50 +0900

    pd_spec_hive fix and package build

--- config/pd_spec_hive.py
+++ config/pd_spec_hive.py
@@ -1,23 +1,25 @@
 #!/bin/sh/env python
 
+import pandera as pa
+
 type_map = {
-    'tinyint':          int,
-    'smallint':         int,
-    'int':              int,
-    'integer':          int,
-    'bigint':           int,
-    'decimal':          float,              # 0.13.0
-    'numeric':          float,              # 3.0.0
-    'float':            float,
-    'double':           float,
-    'double precision': float,              # 2.2.0
-    'boolean':          bool,
-    'char':             str,                # 0.13.0
-    'varchar':          str,                # 0.12.0
-    'binary':           str,                # 0.8.0
-    'date':             str,                # 0.12.0
-    'interval':         str,                # 0.12.0
-    'timestamp':        str,                # 0.8.0
+    'tinyint':          pa.Int8,
+    'smallint':         pa.Int16,
+    'int':              pa.Int32,
+    'integer':          pa.Int32,
+    'bigint':           pa.Int64,
+    'decimal':          pa.Float64,              # 0.13.0
+    'numeric':          pa.Float64,              # 3.0.0
+    'float':            pa.Float32,
+    'double':           pa.Float64,
+    'double precision': pa.Float64,              # 2.2.0
+    'boolean':          pa.Bool,
+    'char':             pa.String,                # 0.13.0
+    'varchar':          pa.String,                # 0.12.0
+    'binary':           pa.String,                # 0.8.0
+    'date':             pa.String,                # 0.12.0
+    'interval':         pa.String,                # 0.12.0
+    'timestamp':        pa.String,                # 0.8.0
     'arrays':           None,               # 0.14
     'maps':             None,               # 0.14
     'structs':          None,
--- setup.py
+++ setup.py
@@ -1,12 +1,30 @@
+import setuptools
 from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
-    name='mypackage',
-    version='0.0.1',
-    packages=['mypackage'],
-    install_requires=[
-        'requests',                                     # 최신버전 설치
-        'pandas >= 3.0',                                # 버전 특정
-        "pywin32 >= 1.0;platform_system=='Windows'",    # 플랫폼 구분
-        'importlib; python_version >= "3.5"',
+    name='migvalidator',
+    version='1.0',
+    author='Gyojun An',
+    author_email='sencom1028@gmail.com',
+    description='Apache Hive and MariaDB(MySQL) conversion library',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
+    install_requires=[
+        'impyla==0.17.0',
+        'pandas==1.3.5',
+        'numpy==1.21.4',
+        'PyMySQL==1.0.2',
+        'pandera==0.8.0',
+        #'pyarrow==6.0.1',
+    ],
+    python_requires='>=3.0',
 )
